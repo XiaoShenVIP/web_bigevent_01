81ed65ac635e804f231ee6392f3d13524e996887
sky <vipshenguanyun@163.com>
2020-10-08 15:51:52 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -7,5 +7,24 @@ var baseURL = "http://ajax.frontend.itheima.net";
 $.ajaxPrefilter(function (params) {
     // 拼接对应环境的服务器地址
     params.url = baseURL + params.url;
-    alert(params.url);
+    // alert(params.url);
+
+    // 2.为请求为/my 开头的所有ajax 配置头信息
+    if (params.url.indexOf('/my/') !== -1) {
+        params.headers = {
+            Authorization: localStorage.getItem("token" || "")
+        }
+    }
+    // 3.身份认证判断
+    params.complete = function (res) {
+        console.log(res);
+        // 权限认证
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            // 1.删除本地 token
+            localStorage.removeItem("token");
+            // 2.页面跳转
+            location.href = '/login.html';
+        }
+    }
+
 });
